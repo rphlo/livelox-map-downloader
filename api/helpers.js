f0440f92e697b5cd611b3d5583e6415f6b45a62d
Raphaël Stefanini <raphael@rphlo.com>
2021-06-26 10:25:27 +0300

    Add kml export

--- api/helpers.js
+++ api/helpers.js
@@ -1,5 +1,7 @@
 const {createCanvas} = require('canvas');
 const { LatLon, cornerCalTransform } = require('./utils');
+const JSZip = require('jszip');
+
 
 const drawRoute = (img, origBounds, routes, res) => {
     const canvas =  createCanvas(Math.round(img.width/res), Math.round(img.height/res));
@@ -77,6 +79,44 @@ const drawRoute = (img, origBounds, routes, res) => {
     return [canvas, bounds];
   };
 
-module.exports = {
-    drawRoute,
-}
+
+const getKml = (name, corners_coords) => {
+    return `<?xml version="1.0" encoding="utf-8"?>
+  <kml xmlns="http://www.opengis.net/kml/2.2"
+        xmlns:gx="http://www.google.com/kml/ext/2.2">
+    <Document>
+      <Folder>
+        <name>${name}</name>
+        <GroundOverlay>
+          <name>${name}</name>
+          <drawOrder>50</drawOrder>
+          <Icon>
+            <href>files/doc.jpg</href>
+          </Icon>
+          <altitudeMode>clampToGround</altitudeMode>
+          <gx:LatLonQuad>
+            <coordinates>
+              ${corners_coords.bottom_left.lon},${corners_coords.bottom_left.lat} ${corners_coords.bottom_right.lon},${corners_coords.bottom_right.lat} ${corners_coords.top_right.lon},${corners_coords.top_right.lat} ${corners_coords.top_left.lon},${corners_coords.top_left.lat}
+            </coordinates>
+          </gx:LatLonQuad>
+        </GroundOverlay>
+      </Folder>
+    </Document>
+  </kml>`;
+  }
+  
+  const saveKMZ = async (name, bound, imgBlob) => {
+    var zip = new JSZip();
+    zip.file("doc.kml", getKml(name, bound));
+    var img = zip.folder("files");
+    img.file("doc.jpg", imgBlob);
+    return zip.generateAsync({type:"nodebuffer"})
+      .then(function(content) {
+          return content;
+      });
+  }
+
+  module.exports = {
+      drawRoute,
+      saveKMZ
+  }
--- api/index.js
+++ api/index.js
@@ -1,52 +1,86 @@
 #! /usr/bin/env node 
-const { loadImage } = require('canvas');
-const url = require('url');
-const fetch = require('node-fetch');
+const { loadImage } = require('canvas')
+const url = require('url')
+const fetch = require('node-fetch')
 const express = require('express')
-const app = express()
-var stream = require('stream');
-const {drawRoute} = require('./helpers')
+const stream = require('stream')
+const { drawRoute, saveKMZ } = require('./helpers')
 
-const main = async (liveloxUrl) => {
-    const classId = url.parse(liveloxUrl, true).query.classId;
-    const res = await fetch("https://www.livelox.com/Data/ClassBlob", {
-        "headers": {
-            "accept": "application/json",
-            "content-type": "application/json",
-        },
-        "body": JSON.stringify({"classIds":[classId],"courseIds":null,"relayLegs":[],"relayLegGroupIds":[],"routeReductionProperties":{"distanceTolerance":1,"speedTolerance":0.1},"includeMap":true,"includeCourses":true,"skipStoreInCache":false}),
-        "method": "POST"
-    });
-    const data = await res.json()
-    const mapUrl = data.map.url;
-    const bound = data.map.boundingQuadrilateral.vertices
-    const resolution = data.map.resolution
-    const route = data.courses.map(c=>c.controls)
-    const mapImg = await loadImage(mapUrl)
-    const [outCanvas, bounds] = drawRoute(mapImg, bound, route, resolution)
-    const filename = `map_${bounds[3].lat}_${bounds[3].lon}_${bounds[2].lat}_${bounds[2].lon}_${bounds[1].lat}_${bounds[1].lon}_${bounds[0].lat}_${bounds[0].lon}_.jpeg`
-    const outBuffer = outCanvas.toBuffer('image/jpeg', 0.8);
-    return [filename, outBuffer];
-}
+const app = express()
 
 const getMap = async (req, res, next) => {
-    const liveloxUrl = req.body.url;
+    const liveloxUrl = req.body.url
     if (!liveloxUrl.startsWith('https://www.livelox.com/')) { 
-        return res.status(400).send('invalid url domain '+liveloxUrl );
+        return res.status(400).send('invalid url domain')
+    }
+    let classId = ''
+    try {
+        classId = url.parse(liveloxUrl, true).query.classId
+    } catch (e) {
+        return res.status(400).send('no class id provided' )
+    }
+    let data = {}
+    try {
+        const res = await fetch("https://www.livelox.com/Data/ClassBlob", {
+            "headers": {
+                "accept": "application/json",
+                "content-type": "application/json",
+            },
+            "body": JSON.stringify({"classIds":[classId],"courseIds":null,"relayLegs":[],"relayLegGroupIds":[],"routeReductionProperties":{"distanceTolerance":1,"speedTolerance":0.1},"includeMap":true,"includeCourses":true,"skipStoreInCache":false}),
+            "method": "POST"
+        });
+        data = await res.json()
+    } catch (e) {
+        return res.status(400).send('could not reach livelox server')
+    }
+    let mapUrl, bound, resolution, route, name
+    try {
+        mapUrl = data.map.url;
+        bound = data.map.boundingQuadrilateral.vertices
+        resolution = data.map.resolution
+        route = data.courses.map(c=>c.controls)
+        name = data.map.name
+    } catch (e) {
+        return res.status(400).send('could not parse livelox data')
     }
     try {
-        const [filename, buffer] = await main(liveloxUrl)
-        var readStream = new stream.PassThrough();
-        readStream.end(buffer);
-        res.set('Content-disposition', 'attachment; filename=' + filename);
-        res.set('Content-Type', 'image/jpeg');
-        readStream.pipe(res);
+        const mapImg = await loadImage(mapUrl)
+        const [outCanvas, bounds] = drawRoute(mapImg, bound, route, resolution)
+        const imgBlob = outCanvas.toBuffer('image/jpeg', 0.8)
+        let buffer
+        let mime
+        let filename
+        if (!req.body.type || req.body.type === 'jpeg') {
+            buffer = imgBlob
+            mime = 'image/jpeg'
+            filename = `${name}_${bounds[3].lat}_${bounds[3].lon}_${bounds[2].lat}_${bounds[2].lon}_${bounds[1].lat}_${bounds[1].lon}_${bounds[0].lat}_${bounds[0].lon}_.jpeg`
+        } else if(req.body.type === 'kmz') {
+            buffer = await saveKMZ(
+                name,
+                {
+                    top_left: bounds[3],
+                    top_right: bounds[2],
+                    bottom_right: bounds[1],
+                    bottom_left: bounds[0]
+                },
+                imgBlob
+            )
+            mime = 'application/vnd.google-earth.kmz'
+            filename = `${name}.kmz`
+        } else {
+            return res.status(400).send('invalid type' )
+        }
+        var readStream = new stream.PassThrough()
+        readStream.end(buffer)
+        res.set('Content-disposition', 'attachment; filename="' + filename.replace(/\\/g, '_').replace(/"/g, '\\"') + '"')
+        res.set('Content-Type', mime)
+        readStream.pipe(res)
     } catch (e) {
-        return res.status(500).send('Something went wrong...');
+        return res.status(500).send('Something went wrong... '+e.message)
     }
 }
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 app.post('/api/get-map', getMap)
